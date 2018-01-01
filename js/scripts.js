60c381ea5d96f6d1bf8d4182de00276298c96880
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 13:47:02 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,57 @@ DropDownListValues.init();
 		function _filter(row) {
 			var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
 
+			row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row'; //for future decoration with css. Currently is not used by javascript or css
+		}
+
+		return {
+			init: function() {
+				var inputs = document.getElementsByClassName('light-table-filter');
+				Arr.forEach.call(inputs, function(input) {
+					input.oninput = _onInputEvent;
+				});
+			}
+		};
+	})(Array.prototype);
+
+	document.addEventListener('readystatechange', function() {
+		if (document.readyState === 'complete') {
+			LightTableFilter.init();
+		}
+	});
+
+
+//Yanire
+
+
+  //21st December Begin
+  var LightTableFilterByColumn = (function(Arr) {
+
+		var _input;
+
+		function _onInputEvent(e) {
+		e.stopPropagation();
+			_input = e.target;
+		//	var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+			var tables = $('.search-table').get(0); //$(_input).closest('.search-table').get(0);
+
+			var arrayFilterColumns = $('.filter-table thead tr th'); //	var arrayFilterColumns = $('.filter-table thead input.searchByColumn');
+			var arrayFilterCriteria =  new Array();
+				//generate an array of all the search criteria in the filter criteria textboxes, in order to consider them all in the filtering process
+				$.each(arrayFilterColumns, function(indice, columnCriteria){
+					//if cell has a column filter textbox or not
+					if($(columnCriteria).find('.searchByColumn').length>0){
+					//6th January
+					arrayFilterCriteria[indice] = { 'criteriaString' : $(columnCriteria).find('.searchByColumn')[0].value.toLowerCase() , 'isEnabled':true}; //arrayFilterCriteria[indice] = columnCriteria.value.toLowerCase();
+				     }
+					 else{
+						arrayFilterCriteria[indice] = { 'criteriaString' : '', 'isEnabled':false}; //arrayFilterCriteria[indice] = columnCriteria.value.toLowerCase();
+					 }
+				});
+
+			$.each(tables.tBodies[0].rows, function(indice, row){
+			var arrayConditionFilter=new Array();
+
 
 
 var navigate = (function() {
